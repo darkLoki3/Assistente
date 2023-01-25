faa6f94c49ef20c2acdd36ed67a1f1d060a2d3d6
Felipe Carvalho Prette <61854726+darkLoki3@users.noreply.github.com>
2023-01-25 11:57:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 from argparse import Action
 from ast import keyword
-import pvporcupine  # módulo sistentizador de voz
-from assistant_functions.weather import Clima  # importa o módulo de temperatura
+import pvporcupine  #module for voice
+from assistant_functions.weather import Clima  #weather module
 # módulo de fala e escuta
-from assistant_functions.Fala_Escuta import Fala_Escuta, Fala_escuta
-from assistant_functions.resposta import resposta  # módulo de resposta
-from assistant_functions.localizacao import Localizacao  # módulo de localização
-from assistant_functions.Abrir_Navegador import NavegadorAssistente  # módulo navegador
-import speech_recognition as sr  # importa o módulo de reconhecimento de fala
-import pyttsx3  # importa o modulo de text para fala
-import pyaudio  # modulo de audio
-# importa o módulo de classificação de intenção
+from assistant_functions.Fala_Escuta import Fala_Escuta, Fala_escuta #speak module
+from assistant_functions.resposta import resposta  #answer module
+from assistant_functions.localizacao import Localizacao  #localization module
+from assistant_functions.Abrir_Navegador import NavegadorAssistente  #browser module
+import speech_recognition as sr  #speech recognition module, but require internet
+import pyttsx3  #text to voice module
+import pyaudio  #audio module
+#intent classification module
 from intent_classification.intent_classification import IntentClassifier
-import struct  # módulo
-import multiprocessing  # módulo de multiprocessamento
+import struct  #struct module
+import multiprocessing  #multiprocessing module
 
-# TODO #END: #4 verificar o código #END
+# TODO #END: #4 Finish review all code #END
 
 
-class Assistant:  # cria a classe assistente
+class Assistant:  # Assistant class
 
-    def __init__(self, name):  # cria a função inicial
-        self.name = name  # cria a instancia nome
+    def __init__(self, name):  # constructor function
+        self.name = name  #self name
 
-    def responde(self, texto):  # responde ao usuário
-        intent = IntentClassifier.predict(texto)  # previsão de respostas
+    def responde(self, texto):  #answer user
+        intent = IntentClassifier.predict(texto)  #try to preview the intent
 
-        respostas = {  # dicionário de respostas
+        respostas = {  #struct answer 
             'despedida': resposta,
             'saudação': resposta,
             'conversa': resposta,
@@ -37,70 +37,70 @@ class Assistant:  # cria a classe assistente
             'abrir no navegador': NavegadorAssistente.main
         }
 
-        responde_func = respostas[intent]  # intenção da fala
+        responde_func = respostas[intent]  #speak intent
 
-        if callable(responde_func):  # verifica se é possivel chamar a resposta
+        if callable(responde_func):  # check if is possible to answer the user
             Fala_escuta.fala(responde_func(texto, intent)
-                             )  # responde ao usuario
+                             )  # answer the user
 
-    def main(self, source):  # função principal
-        print("Pronto")  # avisa que está pronto
-        self.porcupine = None  # atribuí o valor nada
-        pa = None  # atribuí o valor nada para a variável pa
-        audio_stream = None  # atribuí o valor nada a variável audio_stream
+    def main(self, source):  #main function()
+        print("Pronto")  #show the mensage for the user start
+        self.porcupine = None  # put null value
+        pa = None  # put null value for pa var
+        audio_stream = None  # stream audio receive null value
 
-        # palavra que aciona a assistente
+        # keyword for activate the assistant
         self.porcupine = pvporcupine.create(keywords=["Kidy"])
 
-        pa = pyaudio.PyAudio()  # transforma a variável pa em classe pyaudio
+        pa = pyaudio.PyAudio()  # transform the pa var in class for receive audio
 
         audio_stream = pa.open(
             rate=self.porcupine.sample_rate,
             channels=1,
             format=pyaudio.paInt16,
             input=True,
-            frames_per_buffer=self.porcupine.frame_lenght)  # configuração da variável audio_stream
+            frames_per_buffer=self.porcupine.frame_lenght)  # audio stream configuration
 
-        # enquanto for verdade executa os comandos (SEMPRE será verdade)
+        # execute the command while is true (ALWAYS is true)
         while True:
 
-            try:  # tentativa
-                # atribuí a leitura de audio_stream para a variável pcm
+            try:  # try
+                # put audio stream value to pcm var
                 pcm = audio_stream.read(self.porcupine.frame_lenght)
-                # atribuí a largura de frame para pcm
+                # put frame range to pcm
                 pcm = struct.unpack_from(
                     "h" * self.porcupine.frame_lenght, pcm)
-            except:  # exceção
+            except:  # exception
                 audio_stream = pa.open(
                     rate=self.porcupine.sample_rate,
                     channels=1,
                     format=pyaudio.paInt16,
                     input=True,
-                    frames_per_buffer=self.porcupine.frame_lenght)  # configura novamente caso não esteja configurado
+                    frames_per_buffer=self.porcupine.frame_lenght)  # reconfigure buffer if something went wrong
 
-                # cria um indice para a palavra chave
+                # create a index of keyword
                 keyword_index = self.porcupine.process(pcm)
 
-                if keyword_index >= 0:  # verifica o indice
-                    # responde se o indice foi encontrado
+                if keyword_index >= 0:  # Check if index is bigger than zero
+                    # Answer if the index is found
                     print("Palavra quente detectada")
 
-                    try:  # Tenta terminar a ação se ela existe
+                    try:  # try end action if the word exist
                         Action.terminate()  # termina a ação
-                    except:  # excessão
-                        pass  # continua caso não termine a ação
+                    except:  # exception
+                        pass  # pass
 
-                    if audio_stream is not None:  # testa se audio_stream não é vazio
-                        audio_stream.close()  # fecha audio stream
-                    falou = Fala_Escuta.escuta()  # escuta a entrada do usuario
-                    print(falou)  # imprime o que foi dito pelo usuario
+                    if audio_stream is not None:  #Check if audio stream is not null
+                        audio_stream.close()  # close audio stream
+                    falou = Fala_Escuta.escuta()  # listen the user entry
+                    print(falou)  # show what the user say
 
                     #Action = multiprocessing.Process(target=self.responde(falou))
 
-                    self.responde(falou)  # responde ao usuário
+                    self.responde(falou)  # Answer the user
 
 
-# atribuí a classe classificador para a variável classificador
+# put classification to classificater var
 classificadorintencao = IntentClassifier()
-assistente = Assistant('Kidy')  # cria um nome para a assistente
-assistente.main()  # aciona a assistente
+assistente = Assistant('Kidy')  # Create a name for the assistant
+assistente.main()  # run the Assistant
