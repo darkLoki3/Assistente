498e9f87e0abb74415124c10d70dff838de2d218
Felipe Carvalho Prette <61854726+darkLoki3@users.noreply.github.com>
2023-03-17 19:23:01 -0300

    Atualizar o Abrir_Navegador.py

--- assistant_functions/Abrir_Navegador.py
+++ assistant_functions/Abrir_Navegador.py
@@ -28,38 +28,38 @@ class NavegadorAssistente: #classe navegador
             }#dicionário de sites
         Fala_Escuta.fala("Claro!") #responde
         texto = texto.lower() #reduz o texto de caixa alta para caixa baixa
-        for Website_name, Web_address in websites.items(): #percorre os nomes dos sites e endereços que estão nos intems
-            if website_name in texto: #verifica se os nomes dos sites estão no arquivo texto
-                Webbrowser.open_new_tab(web_address) #abreo navegador numa nova aba
+        for Website_name, Web_address in Websites.items(): #percorre os nomes dos sites e endereços que estão nos intems
+            if Website_name in texto: #verifica se os nomes dos sites estão no arquivo texto
+                Webbrowser.open_new_tab(Web_address) #abreo navegador numa nova aba
 
     def Extract_search_term_and_website(self, texto): #função busca termo e site
         texto = texto.lower() #muda de caixa alta para caixa baixa
         texto = texto.replace("Busca por", 'busca') #troca a palavra 'busca por' por 'busca' 
 
         list_of_websites_to_search = ['google', 'kidy'] #lista de sites para busca
-        website_to_search = None #site para busca
-        for website in list_of_websites_to_search: #procura dentro da lista de siste para pesquisa
-            if website in list_of_websites_to_search: #verifica se o site se encontra dentro da lista de sites para pesquisa
-                website_to_search = website #sites para pesquisas recebe site
-                texto = texto.replace(f'on {website}', '') #troca a palavra no dicionário website para vazio
-                texto = texto.replace(f'{website} para', '') #troca a palavra no dicionário website para vazio
+        Website_to_search = None #site para busca
+        for Website in List_of_websites_to_search: #procura dentro da lista de siste para pesquisa
+            if Website in List_of_websites_to_search: #verifica se o site se encontra dentro da lista de sites para pesquisa
+                Website_to_search = wWbsite #sites para pesquisas recebe site
+                texto = texto.replace(f'on {Website}', '') #troca a palavra no dicionário website para vazio
+                texto = texto.replace(f'{Website} para', '') #troca a palavra no dicionário website para vazio
                 break #para
 
         x = Re.search('(?<=busca).*$', texto) #realiza pesquisa
         search_term = x.group().strip() #procura o termo
 
-        if website_to_search is not None and search_term is not None: #verifica se a lista de sites para pesquisa
-            Self.search_and_open(website_to_search, search_term) #pesquisa e abre o site após pesquisas
+        if Website_to_search is not None and Search_term is not None: #verifica se a lista de sites para pesquisa
+            Self.search_and_open(Website_to_search, Search_term) #pesquisa e abre o site após pesquisas
 
-    def Search_and_open(Self, website, search_term): #função busca e abertura
+    def Search_and_open(Self, Website, Search_term): #função busca e abertura
         Fala_Escuta.fala("Claro!") #responde
-        urls_to_search_dict = { 
+        Urls_to_search_dict = { 
             'google' : 'https://www.google.com.br/search?q={}',
             'wikipedia' : 'https://pt.wikipedia.org/wiki/Special:Search/{}',
             'github' : "https://github.com/search?q={}"
             } #dicionário para pesquisa
-        search_url = urls_to_search_dict[website] #pesquisa o endereço dos sites
-        url_to_open = search_url.replace("{}", search_term) #troca o endereço pelo termo de pesquisa
-        webbrowser.open_new_tab(url_to_open) #abre numa aba nova
+        Search_url = Urls_to_search_dict[website] #pesquisa o endereço dos sites
+        Url_to_open = Search_url.replace("{}", search_term) #troca o endereço pelo termo de pesquisa
+        Webbrowser.open_new_tab(Url_to_open) #abre numa aba nova
 
 assistant_browser = NavegadorAssistente() #atribue a classe navegador assistente a variavel
